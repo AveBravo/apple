3880d688b176db49addb44eb60b4fcd236f1c4dc
AveBravo <staronepromo@gmail.com>
2025-10-16 20:15:39 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const navLinks = 
+    [
+        { label: 'Store' },
+        { label: 'Mac' },
+        { label: 'iPad' },
+        { label: 'iPhone' },
+        { label: 'Watch' },
+        { label: 'TV' },
+        { label: 'Music' },
+        { label: 'Support' },
+        { label: 'Account' },
+    ];
